8cf89326f9c8ad3e07d7372a942624b1b7edd4ed
mateocampillo <mateocampillo1@gmail.com>
2023-04-10 11:38:14 -0300

    added static paths to product details

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import HeadComponent from '@/Components/Head';
-import { useRouter } from 'next/router';
 import Sidebar from '@/Components/Sidebar';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 import Footer from '@/Components/Footer';
 
-export default function ProductDetail() {
+interface ratingObject{
+  rate: number;
+  count: number;
+}
+interface apiData{
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: ratingObject;
+}
 
-    const router = useRouter();
-    const { id } = router.query;
+function ProductDetail({data}: {data:apiData}) {
 
   return (
     <>
@@ -16,10 +26,40 @@ export default function ProductDetail() {
       <ProSidebarProvider>
         <Sidebar />
         <main>
-          <h1>producto con id: {id}</h1>
+          <h1>producto {data.title}</h1>
         </main>
         <Footer />
       </ProSidebarProvider>
     </>
   )
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`https://fakestoreapi.com/products`)
+  const data = await res.json()
+  const paths = data.map((product: apiData) => {
+    return {
+      params: {
+        id: `${product.id}`
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context: { params: apiData; }) {
+  const {params} = context
+  const res = await fetch(`https://fakestoreapi.com/products/${params.id}`)
+  const data = await res.json()
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default ProductDetail
