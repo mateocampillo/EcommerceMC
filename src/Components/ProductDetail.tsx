09d8b70fa7bea53e3d5338bff39e9664a908a6ba
mateocampillo <mateocampillo1@gmail.com>
2023-04-12 13:25:44 -0300

    fix h1 product detail

--- src/Components/ProductDetail.tsx
+++ src/Components/ProductDetail.tsx
@@ -47,28 +47,30 @@ export default function ProductDetail(props:propList) {
             ]}/>
         </nav>
         <section className={mulish.className}>
-            <h1 className={styles.h1}>{props.title}</h1>
-            <div className={styles.imgContainer}>
-              <Image alt='imagen de producto' src={props.image} fill objectFit='contain'/>
+          <div className={styles.h1Container}>
+            <h1>{props.title}</h1>
+          </div>
+          <div className={styles.imgContainer}>
+            <Image alt='imagen de producto' src={props.image} fill objectFit='contain'/>
+          </div>
+          <div className={styles.infoContainer}>
+            <p className={styles.description}>{props.description}</p>
+            <div className={styles.divRatingStars}>
+              <RatingStars rating={props.rating.rate} count={props.rating.count}/>
             </div>
-            <div className={styles.infoContainer}>
-              <p className={styles.description}>{props.description}</p>
-              <div className={styles.divRatingStars}>
-                <RatingStars rating={props.rating.rate} count={props.rating.count}/>
+            <div className={styles.amountContainer}>
+              <div className={styles.amount}>
+                <button>-</button>
+                <p>1</p>
+                <button>+</button>
               </div>
-              <div className={styles.amountContainer}>
-                <div className={styles.amount}>
-                  <button>-</button>
-                  <p>1</p>
-                  <button>+</button>
-                </div>
-                <p>Only <strong>{stock}</strong> items left! Don&#39;t miss it!</p>
-              </div>
-              <div className={styles.actionButtons}>
-                <button>Buy Now</button>
-                <button>Add To Cart</button>
-              </div>
+              <p>Only <strong>{stock}</strong> items left! Don&#39;t miss it!</p>
             </div>
+            <div className={styles.actionButtons}>
+              <button>Buy Now</button>
+              <button>Add To Cart</button>
+            </div>
+          </div>
         </section>
     </>
   )
