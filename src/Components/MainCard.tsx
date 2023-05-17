6946f74f315aeb4393aa94323287ffb4c77c80e0
mateocampillo <mateocampillo1@gmail.com>
2023-05-17 12:23:56 -0300

    changed all home css and layout

--- src/Components/MainCard.tsx
+++ src/Components/MainCard.tsx
@@ -3,6 +3,7 @@ import styles from '@/styles/cardComponent.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import Divider from '@mui/material/Divider';
+import { Source_Sans_Pro } from 'next/font/google'
 
 interface propList{
   title: string;
@@ -10,13 +11,15 @@ interface propList{
   category: string;
 }
 
+const ssp = Source_Sans_Pro({weight: ['400'], style: ['normal'], subsets: ['latin']})
+
 export default function CardComponent(props:propList) {
 
   return (
     <div className={styles.container}>
       <div><Link href={`/products?query=${props.category}`} className={styles.containerCardImage}><Image className={styles.cardImage} width={'200'} height={'200'} src={props.image} alt='foto de producto'/></Link></div>
       <Divider variant='middle'/>
-      <div className={styles.containerTitle}><Link href={`/products?query=${props.category}`}><h4>{props.title.toUpperCase()}</h4></Link></div>
+      <div className={[styles.containerTitle, ssp.className].join(" ")}><Link href={`/products?query=${props.category}`}><h4>{props.title.toUpperCase()}</h4></Link></div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,9 @@ import Link from 'next/link';
 import CardComponent from '@/Components/MainCard';
 import Divider from '@mui/material/Divider';
 import Banner from '@/Components/Banner';
+import Image from 'next/image';
+import { Source_Sans_Pro } from 'next/font/google'
+import Logo from '@/Components/Logo';
 
 interface apiData{
   id: number;
@@ -24,6 +27,8 @@ interface apiData{
   rating: object;
 }
 
+const ssp = Source_Sans_Pro({weight: ['400'], style: ['normal'], subsets: ['latin']})
+
 function Home({data}: {data:Array<apiData>}) {
 
   return (
@@ -33,28 +38,56 @@ function Home({data}: {data:Array<apiData>}) {
         <Sidebar />
         <Banner />
         <main>
+          {/* BANNER SECTION */}
           <section>
-            <Card className={styles.card}>
-              <CardMedia className={styles.cardMedia} image={data[14].image} title={data[14].title}/>
+            <div className={styles.bannerImageContainer}>
+              <Image alt='banner main' src={'/img/bcg-home.jpg'} className={styles.bannerImage} width={'500'} height={'500'}/>
+              <div className={[styles.bannerTextContainer, ssp.className].join(" ")}>
+                <h1>StoreHub</h1>
+                <p>Rooted in style</p>
+                <p>Inspiring wardrobe elegance</p>
+              </div>
+            </div>
+            <div className={styles.bannerInfoContainer}>
+              <div className={[styles.bannerLogo, ssp.className].join(" ")}>
+                <Logo color='white'/>
+                <h2>Your online clothing store!</h2>
+              </div>
+              <div className={styles.bannerText}>
+                <p>StoreHub is your ultimate destination for fashion-forward trends and cutting-edge electronics. Join our vibrant community of fashion enthusiasts and tech-savvy individuals.</p>
+                <Link href={'/products?query=all'} className={ssp.className}>Shop all products</Link>
+              </div>
+            </div>
+          </section>
+
+          {/* NEWEST RELEASE */}
+          <section>
+            <Card className={[styles.card, ssp.className].join(" ")}>
+              <CardMedia className={styles.cardMedia} image={data[16].image} title={data[16].title}/>
               <CardContent className={styles.cardContent}>
-                <h2>Product Highlight</h2>
+                <h2>Newest release</h2>
                 <Typography gutterBottom variant="h5" component="div">
-                  {data[14].title}
+                  {data[16].title}
                 </Typography>
               </CardContent>
             </Card>
             <div className={styles.infoContainer}>
-                <Link href={`/products/${data[14].id}`}>Learn more</Link>
+                <Link href={`/products/${data[16].id}`}>Learn more</Link>
               </div>
           </section>
-          <Divider variant='middle'/>
-          <section className={styles.shopCategories}>
-            <h2>Shop Our Top Categories</h2>
+          {/* TOP CATEGORIES */}
+          <div className={styles.categoriesImageContainer}>
+            <Image className={styles.bannerImage} src={'/img/bcg-top-categories-home.jpg'} width={'500'} height={'500'} alt='transition banner'/>
+            <div className={[styles.categoriesTextContainer, ssp.className].join(" ")}>
+                <h2>Our top Categories</h2>
+              </div>
+          </div>
+          <section>
             <div>
-              <CardComponent title={data[1].category} image={data[1].image} category={data[1].category}/>
-              <CardComponent title={data[11].category} image={data[11].image} category={data[11].category}/>
-              <CardComponent title={data[4].category} image={data[4].image} category={data[4].category}/>
               <CardComponent title={data[14].category} image={data[15].image} category={data[15].category}/>
+              <CardComponent title={data[1].category} image={data[1].image} category={data[1].category}/>
+              <CardComponent title={data[4].category} image={data[4].image} category={data[4].category}/>
+              <CardComponent title={data[11].category} image={data[11].image} category={data[11].category}/>
             </div>
           </section>
         </main>
